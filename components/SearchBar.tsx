f296a421e77b0d25fadf198ab0b92fae852dfb3c
a4koshin <mankajr11@gmail.com>
2025-11-02 00:16:00 -0400

    Setup of TMDB API_KEY

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -2,11 +2,16 @@ import { icons } from '@/constants/icons'
 import React from 'react'
 import { Image, StyleSheet, TextInput, View } from 'react-native'
 
-const SearchBar = () => {
+interface props{
+    placeholder?: string,
+    onPressed: ()=>void
+}
+
+const SearchBar = ({placeholder, onPressed}: props) => {
   return (
     <View className='flex-row items-center bg-dark-200  rounded-full px-5 py-4'>
         <Image source={icons.search} className='size-5' resizeMode='contain' tintColor='ab8bff'/>
-        <TextInput onPress={()=>{}}  placeholder='Search' onChangeText={()=>{}} placeholderTextColor='#a8b5db' className='flex-1 ml-2 text-white'/>
+        <TextInput onPress={onPressed}  placeholder={placeholder} onChangeText={()=>{}} placeholderTextColor='#a8b5db' className='flex-1 ml-2 text-white'/>
     </View>
   )
 }
